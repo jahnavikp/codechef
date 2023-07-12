c5428ed91e44233354743dc838e2af075eb39045
Jahnavi_kakkalapalli <107167615+jahnavikp@users.noreply.github.com>
2023-07-12 17:10:23 +0530

    Add files via upload

---
+++ favouritenumbers.py
@@ -0,0 +1,10 @@
+# cook your dish here
+t=int(input())
+for i in range(t):
+    a=int(input())
+    if(a%7==0 and a%2==0):
+        print("Alice")
+    elif(a%9==0 and a%2!=0):
+        print("Bob")
+    else:
+        print("Charlie")
